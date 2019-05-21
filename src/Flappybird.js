2aac33d0dbb744448c59e442be0227fadb56d343
Đặng Lê Minh Trường <dlmtruong1609@gmail.com>
2019-05-21 16:54:36 +0700

    deploy app

---
+++ src/Flappybird.js
@@ -0,0 +1,83 @@
+export default class Flappybird {
+  constructor(context, canvas) {
+    this.ctx = context;
+    this.cvs = canvas;
+
+    this.bird = new Image();
+    this.bg = new Image();
+    this.fg = new Image();
+    this.sewerPipesNorth = new Image();
+    this.sewerPipesSouth = new Image();
+
+    this.bird.src = require("./assets/images/bird.png");
+    this.bg.src = require("./assets/images/bg.png");
+    this.fg.src = require("./assets/images/fg.png");
+    this.sewerPipesNorth.src = require("./assets/images/sewerpipesNorth.png");
+    this.sewerPipesSouth.src = require("./assets/images/sewerpipesSouth.png");
+
+    this.pos = [
+      {
+        x: this.fg.width,
+        y: 0
+      }
+    ];
+    this.space = 300;
+  }
+  getHeight() {
+    return this.cvs.height;
+  }
+
+  getWidth() {
+    return this.cvs.width;
+  }
+
+  drawBird() {
+    this.ctx.drawImage(this.bird, this.getWidth() / 2, this.getHeight() / 2);
+  }
+
+  drawBg() {
+    this.ctx.drawImage(this.bg, 0, 0, this.fg.width, this.getHeight());
+  }
+
+  drawFg() {
+    this.ctx.drawImage(this.fg, 0, this.getHeight() - 100);
+  }
+
+  drawSewerPipesNorth(x, y) {
+    this.ctx.drawImage(this.sewerPipesNorth, x, y);
+  }
+
+  drawSewerPipesSouth(x, y) {
+    this.ctx.drawImage(this.sewerPipesSouth, x, y);
+  }
+
+  draw() {
+    let run = () => {
+      this.drawBg();
+
+      this.drawBird();
+
+      this.drawFg();
+
+      this.drawPipes();
+
+      requestAnimationFrame(run);
+    };
+
+    run();
+  }
+
+  drawPipes() {
+    this.cvs.style.display = "none";
+    for (let i = 0; i < 1; i++) {
+      this.drawSewerPipesNorth(this.pos[i].x, this.pos[i].y);
+      this.drawSewerPipesSouth(this.pos[i].x, this.pos[i].y + this.space);
+
+      this.pos[i].x--;
+
+      if (this.pos[i].x == 50) {
+        this.pos[i].x = 300;
+      }
+    }
+  }
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <canvas ref="canvas" id="canvas" width="400" height="505"></canvas>
+  </div>
+</template>
+
+<script>
+import jQuery from "jquery";
+global.jQuery = jQuery;
+global.$ = jQuery;
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Flappybird from "../Flappybird";
+export default {
+  name: "home",
+  methods: {
+    initFlappy() {
+      let cvs = document.getElementById("canvas");
+      let ctx = cvs.getContext("2d");
+
+      let flappy = new Flappybird(ctx, cvs);
+      window.onload = () => {
+        flappy.draw();
+      };
+    }
+  },
+  mounted() {
+    this.initFlappy();
+  }
+};
+</script>
+<style>
+.container {
+  background: black;
+}
+</style>
